6e8184d35d1f6eadfeaaf866e247102f3bd67907
udbhav1 <umuthakana@gmail.com>
2021-03-12 19:57:25 -0500

    Create main view, allow chart-switching

--- src/renderer.js
+++ src/renderer.js
@@ -1,8 +1,93 @@
 const remote = require("electron").remote;
 const dialog = remote.dialog;
-var Chart = require('chart.js');
 var _ = require('underscore');
 const { ipcRenderer } = require('electron');
+
+var Chart = require('chart.js');
+var chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(231,233,237)'
+};
+
+var color = Chart.helpers.color;
+
+// globals
+var sleepData, labels, dayCounts;
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+var mainChart;
+
+function dayFrequency(){
+    console.log("dayFrequency");
+    // TODO make global pending dynamic update changes
+    var ctx = document.getElementById('mainChart').getContext('2d');
+    // reset canvas
+    if (mainChart != null){
+        mainChart.destroy();
+    }
+    mainChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: days,
+            datasets: [{
+                label: '# of Occurrences',
+                data: dayCounts,
+                // backgroundColor: 'rgba(147, 112, 219, 0.8)',
+                backgroundColor: color(chartColors.red).alpha(0.8).rgbString(),
+                borderWidth: 1
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function sleepQuality(){
+    console.log("sleepQuality");
+    var ctx = document.getElementById('mainChart').getContext('2d');
+    // reset canvas
+    if (mainChart != null){
+        mainChart.destroy();
+    }
+    mainChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            // get only dates
+            labels: _.map(sleepData[labels.indexOf('Start')], function(entry) {
+                return entry[0]; 
+            }),
+            datasets: [{
+                label: "Sleep Quality",
+                data: sleepData[labels.indexOf('Sleep Quality')],
+                backgroundColor: color(chartColors.red).alpha(0.8).rgbString(),
+                borderWidth: 1
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
 
 function removeNaps(data, labels, threshold){
     let ri = labels.indexOf('Time in bed (seconds)');
@@ -55,11 +140,9 @@ function customDomain(data, start, end){
     for(var i = 0; i < dataCopy.length; i++){
         dataCopy[i] = dataCopy[i].slice(startValid, endValid+1);
     }
-    console.log(startValid, endValid);
     return dataCopy;
 }
 
-// TODO write func to get average day of week data
 function dayOfWeekStats(days, data, labels){
     let numericalCols = ['Sleep Quality', 'Regularity', 'Heart rate (bpm)', 'Steps', 'Air Pressure (Pa)', 'Movements per hour', 'Time in bed (seconds)', 'Time asleep (seconds)', 'Time before sleep (seconds)', 'Snore time'];
 
@@ -69,7 +152,6 @@ function dayOfWeekStats(days, data, labels){
         numericalColIndices.push(labels.indexOf(col));
     }
 
-    console.log(numericalColIndices);
     // generate empty array of length days containing arrays of length labels filled with 0s
     let dayData = _.range(days.length).map(function () {
         return _.range(labels.length).map(function () { 
@@ -129,75 +211,31 @@ function getData() {
         // browseBtn.parentNode.replaceChild(spinBtn, browseBtn);
 
         // TODO make async so spinny button renders
-        let labels, sleepData;
         [sleepData, labels] = ipcRenderer.sendSync('synchronous-message', filePath);
-        console.log(sleepData[0]);
-        console.log(sleepData[1]);
-        console.log(labels);
-        document.getElementById("test").innerHTML = "a";
 
         const thresh = 60*120;
         let newData = removeNaps(sleepData, labels, thresh);
-        console.log(sleepData[0].length);
-        console.log(newData[0].length);
 
         let d1 = "05-20-2019";
         let d3 = "06-01-2020";
         let customData = customDomain(sleepData, d1, d3);
-        console.log(sleepData[0].length);
-        console.log(customData[0].length);
 
-        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        let dayCounts = [];
-        [dayCounts, customData] = dayOfWeekStats(days, sleepData, labels);
+        [dayCounts, customData] = dayOfWeekStats(days, removeNaps(sleepData, labels, thresh), labels);
 
-        console.log(dayCounts);
-        console.log(customData);
-        // console.log("Date test:");
-        // let d1 = "05-20-2019";
-        // let d2 = "05-23-2019";
-        // let d3 = "06-01-2020";
-        // console.log(dateGreaterEqual(d1, d2));
-        // console.log(dateGreaterEqual(d1, d3));
-        // console.log(dateGreaterEqual(d2, d3));
-        // console.log(dateGreaterEqual(d2, d1));
-        // console.log(dateGreaterEqual(d3, d1));
-        // console.log(dateGreaterEqual(d3, d2));
+        // remove button, switch to main view
+        const browseBtn = document.getElementById("btnBrowse");
+        browseBtn.remove();
+        console.log(sleepData[1]);
 
-        // console.log(dateGreaterEqual(d1, d1));
-        // console.log(dateGreaterEqual(d2, d2));
-        // console.log(dateGreaterEqual(d3, d3));
-
+        dayFrequency();
         // TODO add all graphs
-        // start date vs quality
+        // start date vs quality (and moving average or similar?)
+        // same for regularity ^
         // duration vs quality
         // histogram of sleep duration
         // plot of time in bed - time asleep (or just two lines)
         // day of week vs quality/duration
         // day of week vs time before sleep
-
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: days,
-                datasets: [{
-                    label: '# of Occurrences',
-                    data: dayCounts,
-                    backgroundColor: _.range(dayCounts.length).map(function () {return 'rgba(147, 112, 219, 0.8)'}),
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
 
     }
 }
